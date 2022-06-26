b8a648efc2089bc34bf7c31d2f3eb180becdcb6e
Tommy Liu <86510873+toommyliu@users.noreply.github.com>
2022-06-25 22:41:33 -0700

    ch6: enums and pattern matching

---
+++ 06_enums/src/main.rs
@@ -0,0 +1,66 @@
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
+fn main() {
+    let value = value_in_cents(Coin::Quarter(UsState::Alaska));
+    println!("{}", value);
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+
+    let dice_roll = 9;
+    match dice_roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        _ => (),
+    }
+
+    let config_max = Some(3u8);
+    match config_max {
+        Some(max) => println!("The maximum is configured to be {}", max),
+        _ => (),
+    }
+
+    let mut count = 0;
+    if let Coin::Quarter(state) = Coin::Penny {
+        println!("State quartr from {:?}", state);
+    } else {
+        count += 1;
+    }
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("lucky penny!");
+            1
+        }
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}", state);
+            25
+        }
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
+fn add_fancy_hat() {}
+fn remove_fancy_hat() {}
